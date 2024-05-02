291d2f793a119a6f92a632e63323a4f3936da4d5
Camilo Alejandro Soto Vega <camilo.alejandro.soto.v@gmail.com>
2024-05-02 12:07:04 -0500

    auth layout and mockup sign in

--- src/layout/AuthLayout.tsx
+++ src/layout/AuthLayout.tsx
@@ -4,7 +4,18 @@ import { Outlet } from "react-router-dom";
 const AuthLayout = () => {
   return (
     <React.Fragment>
-      <Outlet />
+      <div className="bg-light vw-100 vh-100 pt-0 d-flex justify-content-center align-items-center">
+        <div
+          className="container"
+          style={{ minWidth: "75%", minHeight: "50%" }}
+        >
+          <div className="card o-hidden border-0 shadow-lg my-5">
+            <div className="row">
+              <Outlet />
+            </div>
+          </div>
+        </div>
+      </div>
     </React.Fragment>
   );
 };
--- src/pages/auth/SignInPage.tsx
+++ src/pages/auth/SignInPage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
+import { Link } from "react-router-dom";
 
 const SignInPage = () => {
   return (
@@ -7,7 +8,61 @@ const SignInPage = () => {
       <Helmet>
         <title>Sign In | LIMS</title>
       </Helmet>
-      <h1>Sign In</h1>
+      <div className="col-lg-6 d-none d-lg-block bg-signin-image" />
+      <div className="col-lg-6">
+        <div className="p-5">
+          <div className="text-center">
+            <h1 className="h4 text-gray-900 mb-4">Bienvenido!</h1>
+          </div>
+          <form className="user">
+            <div className="form-floating mb-3">
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                placeholder="Email"
+              />
+              <label htmlFor="email">Email</label>
+            </div>
+            <div className="form-floating mb-3">
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                placeholder="Password"
+              />
+              <label htmlFor="email">Password</label>
+            </div>
+
+            <div className="mb-3 form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value=""
+                id="rememberMe"
+              />
+              <label className="form-check-label" htmlFor="rememberMe">
+                Remember me
+              </label>
+            </div>
+
+            <div className="mb-3">
+              <button className="btn btn-primary w-100">Enter</button>
+            </div>
+          </form>
+          <hr />
+          <div className="text-center">
+            <Link className="small" to="/auth/forgot-password">
+              Forgot Password?
+            </Link>
+          </div>
+          <div className="text-center">
+            <Link className="small" to="/auth/sign-up">
+              Create an Account!
+            </Link>
+          </div>
+        </div>
+      </div>
     </React.Fragment>
   );
 };
