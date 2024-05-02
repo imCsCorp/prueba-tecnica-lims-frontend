8ed21630ec92e465f8a5dff50ef63725bbded9f5
Camilo Alejandro Soto Vega <camilo.alejandro.soto.v@gmail.com>
2024-05-02 14:18:26 -0500

    adding validations to input

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -6,6 +6,11 @@ interface Props {
   type?: "text" | "email" | "password";
   id: string;
   label: string;
+  value: string;
+  handleChange: any;
+  handleBlur: any;
+  errors: any;
+  touched: any;
 }
 
 const InputField: FC<Props> = ({
@@ -14,16 +19,26 @@ const InputField: FC<Props> = ({
   name,
   placeholder = "",
   label,
+  value,
+  handleChange,
+  handleBlur,
+  errors,
+  touched,
 }) => {
   return (
     <React.Fragment>
       <div className="form-floating mb-3">
         <input
           type={type}
-          className={`form-control`}
+          className={`form-control is-${
+            touched[id] ? (errors[id] ? "invalid" : "valid") : ""
+          }`}
           id={id}
           name={name ? name : id}
+          value={value}
           placeholder={placeholder}
+          onChange={handleChange}
+          onBlur={handleBlur}
         />
         <label htmlFor={id}>{label}</label>
       </div>
