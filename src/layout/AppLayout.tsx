d85cc2aeeba653e6e2d069d777908124ae40d9ee
Camilo Alejandro Soto Vega <camilo.alejandro.soto.v@gmail.com>
2024-05-02 13:55:52 -0500

    remove token

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -6,11 +6,7 @@ import { AuthContext } from "../context/AuthContext";
 const AppLayout = () => {
   const { auth } = useContext(AuthContext);
   const isAuth: any = useMemo(
-    () =>
-      useAuth(
-        localStorage.getItem("token") ||
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxIiwiZW1haWwiOiJvbGltcHVzbWFjQGdtYWlsLmNvbSIsImlhdCI6MTUxNjIzOTAyMn0.QBIhC2APCv90vN1Cg3V33A3Q9mAlxbLgl1Az111QqdI"
-      ),
+    () => useAuth(localStorage.getItem("token") || ""),
     [auth]
   );
 
