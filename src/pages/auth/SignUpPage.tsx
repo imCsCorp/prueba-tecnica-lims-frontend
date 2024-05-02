1137eb554b4dde3e57c07642437706118dd2f4e4
Camilo Alejandro Soto Vega <camilo.alejandro.soto.v@gmail.com>
2024-05-02 12:14:40 -0500

    mock up sign up

--- src/pages/auth/SignUpPage.tsx
+++ src/pages/auth/SignUpPage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
+import { Link } from "react-router-dom";
 
 const SignUpPage = () => {
   return (
@@ -7,7 +8,77 @@ const SignUpPage = () => {
       <Helmet>
         <title>Sign Up | LIMS</title>
       </Helmet>
-      <h1>Sign Up</h1>
+      <div className="col-lg-6 d-none d-lg-block bg-signup-image" />
+      <div className="col-lg-6">
+        <div className="p-5">
+          <div className="text-center">
+            <h1 className="h4 text-gray-900 mb-4">Crear una cuenta!</h1>
+          </div>
+          <form className="">
+            <div className="form-floating mb-3">
+              <input
+                type="text"
+                className="form-control"
+                id="firtsName"
+                placeholder="Nombres"
+              />
+              <label htmlFor="firtsName">Nombres</label>
+            </div>
+            <div className="form-floating mb-3">
+              <input
+                type="text"
+                className="form-control"
+                id="lastName"
+                placeholder="Apellidos"
+              />
+              <label htmlFor="lastName">Apellidos</label>
+            </div>
+            <div className="form-floating mb-3">
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                placeholder="Correo electronico"
+              />
+              <label htmlFor="email">Correo electronico</label>
+            </div>
+            <div className="form-floating mb-3">
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                placeholder="Contraseña"
+              />
+              <label htmlFor="email">Contaseña</label>
+            </div>
+
+            <div className="mb-3 form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value=""
+                id="terms"
+              />
+              <label className="form-check-label" htmlFor="terms">
+                Acepto los terminos y condiciones.
+              </label>
+            </div>
+
+            <div className="mb-3">
+              <button className="btn btn-primary w-100">Registrarme</button>
+            </div>
+          </form>
+          <hr />
+          <div className="text-center">
+            <p>
+              Ya tienes cuenta{" "}
+              <Link className="small" to="/auth/sign-up">
+                ingresa aqui
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
     </React.Fragment>
   );
 };
