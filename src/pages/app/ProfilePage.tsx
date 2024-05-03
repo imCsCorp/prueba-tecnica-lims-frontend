d213eeb98a78554746fe9fe4278ea6f10db40b2e
Camilo Alejandro Soto Vega <camilo.alejandro.soto.v@gmail.com>
2024-05-03 06:42:37 -0500

    get profile data

--- src/pages/app/ProfilePage.tsx
+++ src/pages/app/ProfilePage.tsx
@@ -1,13 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Helmet } from "react-helmet-async";
+import axiosInterceptor from "../../config/axiosInterceptor";
+import axiosClient from "../../config/axiosClient";
+import { BadRequest } from "../../utils/responses";
+import { User } from "../../interfaces/user";
 
 const ProfilePage = () => {
+  const [user, setUser] = useState<User>({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+  useEffect(() => {
+    axiosInterceptor();
+    axiosClient
+      .get("user/profile")
+      .then(({ data }) => {
+        setUser(data);
+      })
+      .catch(BadRequest);
+  }, []);
+
   return (
     <React.Fragment>
       <Helmet>
         <title>Profile | LIMS</title>
       </Helmet>
-      <h1>Profile</h1>
+      <div className="card">
+        <div className="card-body">
+          <h1 className="card-title h5 text-center mb-4">Mis datos</h1>
+          <div className="mb-3">
+            <p>Nombres: {user.firstName}</p>
+          </div>
+          <div className="mb-3">
+            <p>Apellidos: {user.lastName}</p>
+          </div>
+          <div className="mb-3">
+            <p>Correo electronico: {user.email}</p>
+          </div>
+          <div className="mb-3">
+            <p>Estado: {user.active ? "Activo" : "Inactivo"}</p>
+          </div>
+        </div>
+      </div>
     </React.Fragment>
   );
 };
